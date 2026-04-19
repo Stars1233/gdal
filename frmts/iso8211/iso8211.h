a719f76095351077f998df166c643ba3bee0ef42
Even Rouault <even.rouault@spatialys.com>
2026-04-20 00:41:31 +0200

    iso8211: add DDFRecord::GetFields()

--- frmts/iso8211/iso8211.h
+++ frmts/iso8211/iso8211.h
@@ -614,6 +614,8 @@ class CPL_DLL DDFRecord
 
     const DDFField *GetField(int) const;
 
+    std::vector<const DDFField *> GetFields(const char *pszFieldName) const;
+
     const std::vector<DDFField> &GetFields() const
     {
         return aoFields;
